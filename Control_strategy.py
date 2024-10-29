7cb53b2c667a4bf7e0f691dbd2a083ffd32c736b
SebsCubs <ascespedesc@unal.edu.co>
2024-10-29 14:16:12 +0100

    Organizing control policy training, adding comments

--- Control_strategy.py
+++ Control_strategy.py
@@ -7,12 +7,6 @@ Integrate the cost function and constraints.
 Solve the control problem at each timestep.
 Graphviz is needed in system PATH to run the simulation. Important with problem.show()
 '''
-import sys
-# import io
-# sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
-# # sys.stdout.reconfigure(encoding='utf-8')
-
-# # -*- coding: utf-8 -*-
 
 import numpy as np
 import torch
@@ -20,8 +14,6 @@ import pickle
 import torch.nn as nn
 from neuromancer.modules import blocks
 from matplotlib import pyplot as plt
-import networkx as nx
-
 from neuromancer.system import System, Node
 from neuromancer.problem import Problem
 from neuromancer.trainer import Trainer
@@ -33,143 +25,115 @@ from torch.utils.data import DataLoader, TensorDataset
 from neuromancer.dataset import DictDataset
 
 
+def load_closed_loop_system(nx, nu, show=False):
 
-## Retrieving of data
+    nssm_node = model_loading()
+    net = blocks.MLP_bounds(
+        insize=nx, 
+        outsize=nu,  
+        hsizes=[32, 32],                
+        nonlin=nn.GELU,  
+        min=torch.tensor([[0.0]]),  # Fan speed minimum
+        max=torch.tensor([[1.0]]),  # Fan speed maximum
+    )
 
-with open('test_data.pkl', 'rb') as f:
-    test_data = pickle.load(f)
-
-nssm_node = model_loading()
-
-
-# train_data = TensorDataset(test_data['X'], test_data['U'])
-train_data = DictDataset({'X': test_data['X'], 'U': test_data['U']})
-train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
-dev_loader = DataLoader(train_data, batch_size=32)
-
-#Define the Variables
-
-u = variable("U")
-x = variable("X")
-xhat = variable('xn')[:, :-1, :]
-CO2 = variable('xn')[:, :-1, 2] 
-P_fan = variable('xn')[:, :-1, 4]
-temp_soft = variable('xn')[:, :-1, 0]
-
-nsteps = 50
-nx = test_data['X'].shape[2]                   #Number of states
-nu = test_data['U'].shape[2]                   #Number of inputs
-
-# Define constants 
-CO2_setpoint = 500                                  # Setpoint for CO2
-alpha = 0.1                                         # Weighting factor for energy consumption ??
-electricity_cost = np.array([602.79, 620.69, 611.67, 620.54, 639.86, 675.95, 780.95, 1025.33, 1205.73, 957.69, 
-                             776.33, 693.70, 600.93, 574.75, 562.45, 657.83, 696.23, 1009.67, 1428.86, 1312.52, 
-                             833.08, 754.85, 767.60, 694.74])                  # 24-hour electricity prices (in DKK/MWh)
-CO2_min, CO2_max = 400, 1000                        # CO2 concentration bounds
-T_min, T_max = 18, 25                               # Temperature bounds
-U_min, U_max = 0, 1.35                                # Fan speed bounds
-
-# CO2 = test_data['X'][:, :, 2]
-Temp_soft = test_data['X'][:, :, 0]
-U = test_data['U'][:, :, 0]
-
-
-
-#Control policy 
-
-net = blocks.MLP_bounds(
-    insize=nx,  # Size of system state 
-    outsize=nu,  
-    hsizes=[32, 32],                
-    nonlin=nn.GELU,  
-    min=torch.tensor([[0.0]]),  # Fan speed minimum
-    max=torch.tensor([[1.0]]),  # Fan speed maximum
-)
-
-def train_control_policy():
-
-    policy = Node(net, ['X'], ['U'], name='control_policy')  # Control policy node
-
+    policy = Node(net, ['xn'], ['U'], name='control_policy')  # Control policy node
     nssm_node.freeze()
     cl_system = System([policy, nssm_node], name='cl_system', nsteps=nsteps)
 
-    # cl_system.show()
+    if show:
+        cl_system.show()
+
+    return cl_system
+
+def load_training_data():
+
+    """
+    You want to reformulate the way you're creating the training data.
+    You should have a train_loader with the keys 'xn' and 'U' (X is not needed, the policy should use only the estimates from the NSSM)
+    The NSSM will need initial conditions 'xn' for each batch. 
+    Therefore the shape of those tensors should be (batch_size, 1, nx) and (batch_size, nsteps, nu) respectively.
+    Remember you need to divide the data properly, as it is now the train and dev data are the same.
+    You would also want to split the data into trai, dev and test data, where the test data you would use after training to evaluate the control policy.
+    """
 
 
+    with open('test_data.pkl', 'rb') as f:
+        test_data = pickle.load(f)
+
+    train_data = DictDataset({'x': test_data['X'], 'U': test_data['U']})
+    train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
+    dev_loader = DataLoader(train_data, batch_size=32)
+    
+    nx = test_data['X'].shape[2]                   #Number of states
+    nu = test_data['U'].shape[2]                   #Number of inputs
+
+    return train_loader, dev_loader, nx, nu
+
+
+
+
+
+
+
+def train_control_policy(cl_system : System, nsteps, train_loader, dev_loader, show=False):
 
     '''
     Below the cost function, constrains and optimisation of problem
     '''
     ############### Cost function ###################
     # cost_function = (CO2 - CO2_setpoint)**2 + alpha * P_fan * (electricity_cost[0] / 1000)  # Only using first electricity cost value for now
+    #Define the symbolic variables for the control policy
+    u = variable("U")
+    x = variable("xn")
+    xhat = variable('xn')[:, :-1, :]
+    CO2 = variable('xn')[:, :-1, 2] 
+    P_fan = variable('xn')[:, :-1, 4]
+    temp_soft = variable('xn')[:, :-1, 0]
+    
+    obj_1 = ((CO2 - CO2_setpoint)**2).minimize()
+    obj_1.name = "CO2_loss"
 
-    CO2_loss= (CO2 - CO2_setpoint)**2
-    energy_loss =  P_fan * (electricity_cost[0] / 1000)
+    obj_2 =  ((electricity_cost[0]/1000)*P_fan).minimize()
+    obj_2.name = "Energy_loss"
 
-    # define the objectives
-    obj_1 = CO2_loss.minimize()
-    obj_1.name = 'CO2_loss'
-    obj_2 = energy_loss.minimize()
-    obj_1.name = 'Energy_loss'
 
     
     objectives = [obj_1, obj_2]
 
     # Define the constrains
     U_min_constraint = (u >= U_min)
-    U_min_constraint.name = 'Input_Min'
+    U_min_constraint.name = "Input_Min"
 
     U_max_constraint = (u <= U_max)
-    U_max_constraint.name = 'Input_Max'
+    U_max_constraint.name = "Input_Max"
 
     CO2_min_constraint = (CO2 >= CO2_min)
-    CO2_min_constraint.name = 'CO2_min'
+    CO2_min_constraint.name = "CO2_min"
 
     CO2_max_constraint = (CO2 <= CO2_max)
-    CO2_max_constraint.name = 'CO2_Max'
+    CO2_max_constraint.name = "CO2_Max"
 
     T_min_constraint = (temp_soft >= T_min)
-    T_min_constraint.name = 'Temp_min'
+    T_min_constraint.name = "Temp_min"
 
     T_max_constraint = (temp_soft <= T_max)
-    T_max_constraint.name = 'Temp_Max'
+    T_max_constraint.name = "Temp_Max"
 
 
     constraints = [U_min_constraint, U_max_constraint, 
                    CO2_min_constraint, CO2_max_constraint, 
                    T_min_constraint, T_max_constraint]
 
-
-    # create constrained optimization loss
     loss = PenaltyLoss(objectives, constraints)
-    # construct constrained optimization problem
     problem = Problem([cl_system], loss)
+    
+    if show:
+        problem.show()
 
-
-    # with open("problem_graph.txt", "w", encoding="utf-8") as f:
-        # sys.stdout = f
-    # problem.show("problem_graph.png")
-
-    graph = problem.graph()  # Create graph structure directly
-    graph.write_png('problem_graph_direct.png')
-    # graph = problem.get_graph()
-    # nx.draw(graph, with_labels=True)
-    # plt.savefig("problem_graph.png")
-    # plt.close()    
-
-    cl_system.nsteps = test_data['X'].shape[1]
-    test_outputs = cl_system(test_data)
-    print(test_outputs['xn'])
-
-    # import networkx as nx
-    # # Instead of problem.show()
-    # graph = problem.get_graph()  # Get the computational graph
-    # nx.write_gml(graph, "problem_graph.gml")  # Write the graph to a file (GML format)
-
+    cl_system.nsteps = nsteps # Coming from the constant set in the main block
 
     optimizer = torch.optim.AdamW(problem.parameters(), lr=0.001)
-    #  Neuromancer trainer
     trainer = Trainer(
         problem,
         train_loader,
@@ -193,30 +157,25 @@ def train_control_policy():
 
     return cl_system
 
-cl_system = train_control_policy()
+if __name__ == '__main__':
 
-cl_system.show()
-### Evaluation of model
-# rmse = np.sqrt(mean_squared_error(true_traj.flatten(), pred_traj.flatten()))
-# print(f'RMSE: {rmse}')
-
-# mae = mean_absolute_error(true_traj.flatten(), pred_traj.flatten())
-# print(f'MAE: {mae}')
-
-# r2 = r2_score(true_traj.flatten(), pred_traj.flatten())
-# print(f'R² score: {r2}')
-
-# rmse_states = np.sqrt(mean_squared_error(true_traj.flatten(), pred_traj.flatten()))
-# rmse_inputs = np.sqrt(mean_squared_error(test_data['U'].detach().numpy().flatten(), input_traj.flatten()))
-
-# print(f'RMSE for States: {rmse_states}')
-# print(f'RMSE for Inputs: {rmse_inputs}')
+    # Define constants 
+    CO2_setpoint = 500                                  # Setpoint for CO2
+    alpha = 0.1                                         # Weighting factor for energy consumption ??
+    electricity_cost = np.array([602.79, 620.69, 611.67, 620.54, 639.86, 675.95, 780.95, 1025.33, 1205.73, 957.69, 
+                                776.33, 693.70, 600.93, 574.75, 562.45, 657.83, 696.23, 1009.67, 1428.86, 1312.52, 
+                                833.08, 754.85, 767.60, 694.74])                  # 24-hour electricity prices (in DKK/MWh)
+    CO2_min, CO2_max = 400, 1000                        # CO2 concentration bounds
+    T_min, T_max = 18, 25                               # Temperature bounds
+    U_min, U_max = 0, 1.35                                # Fan speed bounds
+    nsteps = 50
 
 
+    #### Main program execution ####
 
-# print(test_outputs['xn'])
-# print(f'Shape of the test data [xn]: {test_data["xn"].shape}')
-# # print(test_data['xn'])
-# print(f'Shape of the test data [U]: {test_data["U"].shape}')
-# # print(test_data['U'])
+    train_loader, dev_loader, nx, nu = load_training_data()
+    cl_system = load_closed_loop_system(nx , nu, show=True)
+    cl_system = train_control_policy(cl_system=cl_system, nsteps=nsteps, train_loader=train_loader, dev_loader=dev_loader, show=True)
+
+
 
